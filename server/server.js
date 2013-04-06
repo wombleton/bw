42da44d0194a884a135d49e89683fe9250189061
Rowan Crawford <wombleton@gmail.com>
2013-04-06 14:29:41 +1300

    Publish characters belonging to user

--- server/server.js
+++ server/server.js
@@ -1,10 +1,29 @@
-// All Tomorrow's Parties -- server
+// Hardholder-- server
 
-Meteor.publish("directory", function () {
-  return Meteor.users.find({}, {fields: {emails: 1, profile: 1}});
+Meteor.publish("directory", function() {
+    return Meteor.users.find({}, {
+        fields: {
+            emails: 1,
+            profile: 1
+        }
+    });
 });
 
-Meteor.publish("games", function () {
-  return Games.find(
-    {$or: [{"public": true}, {invited: this.userId}, {owner: this.userId}]});
+Meteor.publish("games", function() {
+    return Games.find({
+        $or: [
+            {
+                players: this.userId
+            },
+            {
+                owner: this.userId
+            }
+        ]
+    });
+});
+
+Meteor.publish("characters", function() {
+    return Characters.find({
+        owner: this.userId
+    });
 });
