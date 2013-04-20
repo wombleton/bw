bc6c22aa65917be5b4d9618fa578a053ca1340eb
Rowan Crawford <wombleton@gmail.com>
2013-04-21 00:29:54 +1200

    active refactor

--- client/active.js
+++ client/active.js
@@ -1,4 +1,39 @@
-Template.active.description = function() {
-    return Games.findOne(Session.get('active')).description;
+function getGame() {
+    return Games.findOne(Session.get('active'));
+}
+Template.active.game = function() {
+    return getGame();
 };
 
+Template.active.role = function() {
+    var game = getGame(),
+        userId = Meteor.userId();
+
+    if (game.gm === userId) {
+        return 'gm';
+    } else if (_.contains(game.players, userId)) {
+        return 'player';
+    }
+}
+
+Template.active.events({
+    'click [data-role]': function(e) {
+        var game = getGame(),
+            role = $(e.currentTarget).attr('data-role'),
+            userId = Meteor.userId();
+
+        if (role === 'gm') {
+            Games.update(Session.get('active'), {
+                $set: {
+                    gm: userId
+                }
+            });
+        } else {
+            Games.update(Session.get('active'), {
+                $push: {
+                    players: userId
+                }
+            });
+        }
+    }
+});
