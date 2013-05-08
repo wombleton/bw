fb5b0dd3f11c39e6f5a046f46506947b404afec6
Rowan Crawford <wombleton@gmail.com>
2013-05-08 17:52:28 +1200

    publish games too

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ Meteor.publish("directory", function() {
     return Meteor.users.find({}, {
         fields: {
             emails: 1,
+            game: 1,
             profile: 1
         }
     });
