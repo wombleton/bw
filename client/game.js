9c1a8658af9e3ac868c0ed0a63d61ef8f762e72d
Rowan Crawford <wombleton@gmail.com>
2013-05-15 19:09:48 +1200

    remove unused code, move sheet to own files

--- client/game.js
+++ client/game.js
@@ -9,16 +9,3 @@ Template.game.player = function() {
         return player.id === userId;
     });
 };
-
-Template.game.character = function() {
-    var player = Template.game.player();
-
-    return player && player.character;
-}
-
-Template.game.events({
-    'click button': function() {
-        Game.
-        Meteor.call('setupCharacter', Meteor.userId());
-    }
-});
