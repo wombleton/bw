6ce4bfeb529a1e487723286ca28ce4fdf3d22c6a
Rowan Crawford <wombleton@gmail.com>
2013-06-26 20:47:39 +1200

    Reference game from sheet

--- client/views/editableSheet.js
+++ client/views/editableSheet.js
@@ -4,10 +4,14 @@ Meteor.autorun(function() {
 
 Template.editableSheet.sheet = function() {
     return Characters.findOne(Session.get('sheetId'));
-}
+};
 
 Template.editableSheet.stats = function() {
     return Stats.find({
         sheetId: Session.get('sheetId')
     });
-}
+};
+
+Template.editableSheet.game = function() {
+    return Games.findOne(Template.editableSheet.sheet().gameId);
+};
