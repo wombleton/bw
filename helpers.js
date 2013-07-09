2e4179cd6ca38508ab342d7c6667c15252d586cb
Rowan Crawford <wombleton@gmail.com>
2013-07-09 20:04:49 +1200

    don't need statlist helper

--- helpers.js
+++ helpers.js
@@ -6,8 +6,4 @@ if (Meteor.isClient) {
     Handlebars.registerHelper('selected', function(a, b) {
       return String(a) === String(b) ? ' selected' : '';
     });
-
-    Handlebars.registerHelper('statList', function() {
-        return JSON.stringify(Skills.find({}).map(function (x) { return x.name; })).replace(/"/g, '&quot;');
-    })
 }
